2bb4c32038028521b2700fae8b4fc6c5972c0e7c
“sstone72389” <sstone72389@gmail.com>
2017-06-20 12:50:06 -0400

    Adds modals for auth
    backdrop not disappearing. there appears to be a conflict in events.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,11 +2,13 @@
 const store = require('../store.js')
 
 const signUpSuccess = (data) => {
+  $('#signUpModal').modal('hide')
   $('input').val('')
 }
 
 const signUpFailure = (response) => {
   $('.text-display').text('Error Signing Up')
+  $('#signUpModal').modal('hide')
   $('input').val('')
 }
 
@@ -17,12 +19,17 @@ const signInSuccess = (data) => {
   $('input').val('')
   $('.cartHas-display').empty()
   $('.purchase-display').empty()
-  $('.sign-in-area').addClass('hidden')
-  $('#change-pwd').removeClass('hidden')
-  $('#sign-out').removeClass('hidden')
+  // $('.sign-in-area').addClass('hidden')
+  // $('#change-pwd').removeClass('hidden')
+  // $('#sign-out').removeClass('hidden')
   $('.purchaseConfirm').text('')
   $('.cartHas-display').hide()
   $('.purchase-display').hide()
+  $('.hideOnStart').show()
+  $('.hideOnStartTwo').show()
+  $('.hideOnSignIn').hide()
+  $('.hideOnSignInTwo').hide()
+  $('#signInModal').modal('hide')
   document.getElementById('sign-in').reset()
 
   // store the user object as per belows
@@ -31,19 +38,22 @@ const signInSuccess = (data) => {
 
 const signInFailure = () => {
   $('.text-display').text('Error Signing In')
+  $('#signInModal').modal('hide')
   $('input').val('')
 }
 
 const changePasswordSuccess = (data) => {
   $('.text-display').text('Password Changed Successfully').fadeIn().delay(2000).fadeOut('slow')
-  $('#change-password').addClass('hidden')
-  document.getElementById('change-password').reset()
+  // $('#change-password').addClass('hidden')
+  // document.getElementById('change-password').reset()
+  $('#changePasswordModal').modal('hide')
   $('input').val('')
 }
 
 const changePasswordFailure = () => {
   $('.text-display').text('Error Changing Password').fadeIn().delay(2000).fadeOut('slow')
-  document.getElementById('change-password').reset()
+  // document.getElementById('change-password').reset()
+  $('#changePasswordModal').modal('hide')
   $('input').val('')
 }
 
@@ -51,38 +61,43 @@ const signOutSuccess = (data) => {
   $('.text-display').text('Thanks for shopping. Come back soon!')
   $('.text-display').show()
   $('.hideBeforeLogin').hide()
-  $('.sign-in-area').removeClass('hidden')
-  $('#log-in').removeClass('hidden')
-  $('#register').removeClass('hidden')
-  $('#change-pwd').addClass('hidden')
-  $('#change-password').addClass('hidden')
-  $('#sign-out').addClass('hidden')
-  if (!$('#sign-in').hasClass('hidden')) {
-    $('#sign-in').addClass('hidden')
-  }
-  if (!$('#sign-up').hasClass('hidden')) {
-    $('#sign-up').addClass('hidden')
-  }
-  if (!$('.change-pwd-success').hasClass('hidden')) {
-    $('.change-pwd-success').addClass('hidden')
-  }
-  if (!$('.change-pwd-error').hasClass('hidden')) {
-    $('.change-pwd-error').addClass('hidden')
-  }
+  $('.hideOnStart').hide()
+  $('.hideOnStartTwo').hide()
+  $('.hideOnSignIn').show()
+  $('.hideOnSignInTwo').show()
+  $('#signOutModal').modal('hide')
+  // $('.sign-in-area').removeClass('hidden')
+  // $('#log-in').removeClass('hidden')
+  // $('#register').removeClass('hidden')
+  // $('#change-pwd').addClass('hidden')
+  // $('#change-password').addClass('hidden')
+  // $('#sign-out').addClass('hidden')
+  // if (!$('#sign-in').hasClass('hidden')) {
+  //   $('#sign-in').addClass('hidden')
+  // }
+  // if (!$('#sign-up').hasClass('hidden')) {
+  //   $('#sign-up').addClass('hidden')
+  // }
+  // if (!$('.change-pwd-success').hasClass('hidden')) {
+  //   $('.change-pwd-success').addClass('hidden')
+  // }
+  // if (!$('.change-pwd-error').hasClass('hidden')) {
+  //   $('.change-pwd-error').addClass('hidden')
+  // }
   // store the user with a value of null as per below
   store.user = null
 }
 
 const signOutFailure = (response) => {
   $('.text-display').text('Sorry, unable to sign out.')
+  $('#signOutModal').modal('hide')
 }
 
 const emptyCartSuccess = (data) => {
   store.buyerId = data.buyer._id
 }
 
-const emptyCartFailure = (response) => {
-}
+const emptyCartFailure = (response) => {}
 
 module.exports = {
   signUpSuccess,
